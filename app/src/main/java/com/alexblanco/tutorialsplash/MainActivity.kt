9801ded7a27541df5bab14aadaefbd3a3da40fac
AlexBlancoRamos <alex.blanco@institutvidreres.cat>
2023-11-06 09:18:30 +0100

    Initial commit

---
+++ app/src/main/java/com/alexblanco/tutorialsplash/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.alexblanco.tutorialsplash
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
